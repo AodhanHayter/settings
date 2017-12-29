5b43c44fd8db16ddfea62842334a8280c2016be9
Aodhan Hayter <aodhan.hayter@gmail.com>
2017-12-29 10:14:42 -0700

    Add vim setup script

--- vim-setup.sh
+++ vim-setup.sh
@@ -3,12 +3,16 @@
 set -euo pipefail
 
 VIM_HOME="$HOME/.vim"
+AUTOLOAD_HOME="$VIM_HOME/autoload"
 VIMRC_PATH="$HOME/settings/dotfiles/vim/vimrc"
+VIM_PLUG_PATH="$HOME/settings/dotfiles/vim/autoload/plug.vim"
 
 if [ ! -d "$VIM_HOME" ]; then
   mkdir -v $VIM_HOME
+  mkdir -v $AUTOLOAD_HOME
 else
   echo ".vim directory already setup"
 fi
 
 ln -sv $VIMRC_PATH "$VIM_HOME/vimrc"
+ln -sv $VIM_PLUG_PATH "$AUTOLOAD_HOME/plug.vim"
