197cf9724f92b13e1cda60f52dea9f5b8cc3d568
Aodhan Hayter <aodhan.hayter@gmail.com>
2018-07-11 22:00:25 -0600

    Add global config for eslint

---
+++ dotfiles/runcom/eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  env: {
+    es6: true,
+    browser: true,
+    commonjs: true,
+  },
+  extends: ['eslint:recommended'],
+}
