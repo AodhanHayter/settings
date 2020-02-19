f6a499c9e214ec2f68fcb72358877afba9d3d141
Aodhan Hayter <aodhan.hayter@gmail.com>
2020-02-19 00:37:08 -0700

    Fix vim setup and settings

--- vim-setup.sh
+++ vim-setup.sh
@@ -3,16 +3,15 @@
 set -euo pipefail
 
 VIM_HOME="$HOME/.vim"
-AUTOLOAD_HOME="$VIM_HOME/autoload"
+VIM_AUTOLOAD_HOME="$VIM_HOME/autoload"
 VIMRC_PATH="$HOME/settings/dotfiles/vim/vimrc"
 VIM_PLUG_PATH="$HOME/settings/dotfiles/vim/autoload/plug.vim"
 
 if [ ! -d "$VIM_HOME" ]; then
   mkdir -v $VIM_HOME
-  mkdir -v $AUTOLOAD_HOME
+  mkdir -v $VIM_AUTOLOAD_HOME
 else
   echo ".vim directory already setup"
 fi
 
 ln -sv $VIMRC_PATH "$VIM_HOME/vimrc"
-ln -sv $VIM_PLUG_PATH "$AUTOLOAD_HOME/plug.vim"
